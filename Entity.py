dc7e075ae107ee39beefa44c5688363207f8a890
linzion <linzino7@gmail.com>
2017-08-26 13:48:02 +0800

    init

---
+++ Entity.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+@author: linzino
+
+fefe
+"""
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+
+class PictureDate(db.Model):
+
+    __tablename__ = 'usermessage'
+
+    id = db.Column(db.String(50), primary_key=True)
+    user_id = db.Column(db.String(50))
+    message = db.Column(db.Text)
+    birth_date = db.Column(db.TIMESTAMP)
+
+if __name__ == '__main__':
+    manager.run()
